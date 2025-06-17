396b1a911e3e80db1f1999ad94c0f5bd5f0fec60
Zhuowen-Chen <zhuowen.chen@ucdconnect.ie>
2025-06-17 23:59:08 +0100

    edit WhoWeAre.jsx and its css

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-// import WhoWeAre from './components/WhoWeAre';
+import WhoWeAre from './components/WhoWeAre';
 // import Welcome from './components/Welcome';
 // import WhyChooseUs from './components/WhyChooseUs';
 // import HowItWorks from './components/HowItWorks';
@@ -13,8 +13,8 @@ function App() {
     <div className="App">
       <Navbar />
       <Hero />
-      {/* <WhoWeAre />
-      <Welcome />
+      <WhoWeAre />
+      {/* <Welcome />
       <WhyChooseUs />
       <HowItWorks />
       <Footer /> */}
