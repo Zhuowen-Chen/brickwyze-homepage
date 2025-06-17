ff0b937c70ef9ce9e590f749c731f9cde8b86bdd
Zhuowen-Chen <zhuowen.chen@ucdconnect.ie>
2025-06-17 17:27:35 +0100

    edit css

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
-import backgroundImg from '../assets/images/Background.jpg'; // 推荐方式
+import backgroundImg from '../assets/images/Background.jpg';
 
 const Hero = () => {
   return (
     <section className="hero">
-      <h1>Land Your Biz Where It Pops.</h1>
-      <h2>Real data. Real blocks. Real results.</h2>
-
       <img src={backgroundImg} alt="ManhattanBusinessBackgroundImage" className="hero-background" />
-
-      <button 
-        className="map-button" 
-        onClick={() => { /* 后续添加跳转逻辑 */ }}
+      
+      <div className='hero-content'>
+        <h1>Land Your Biz Where It Pops.</h1>
+        <h2>Real data. Real blocks. Real results.</h2>  
+        
+        <button 
+            className="map-button" 
+            onClick={() => { /* 后续添加跳转逻辑 */ }}
         >
 
-        Hit the Map
-      </button>
+            Hit the Map
+        </button>
+
+      </div>
     </section>
   );
 };
