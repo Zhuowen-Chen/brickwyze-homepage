26d63e3cb464e521a9ee1cd2bce601d3472b290f
Zhuowen-Chen <zhuowen.chen@ucdconnect.ie>
2025-06-17 15:16:48 +0100

    reorganized all files and edit App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+// src/App.jsx
+import './App.css';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import WhoWeAre from './components/WhoWeAre';
+import Welcome from './components/Welcome';
+import WhyChooseUs from './components/WhyChooseUs';
+import HowItWorks from './components/HowItWorks';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <WhoWeAre />
+      <Welcome />
+      <WhyChooseUs />
+      <HowItWorks />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
