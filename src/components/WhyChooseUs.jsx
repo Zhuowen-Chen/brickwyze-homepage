b56312afb0aa307efac3e0901f82896a32c46db5
Zhuowen-Chen <zhuowen.chen@ucdconnect.ie>
2025-06-18 22:16:20 +0100

    edit whychooseus jsx

--- src/components/WhyChooseUs.jsx
+++ src/components/WhyChooseUs.jsx
@@ -1,4 +1,24 @@
 import React from "react";
 import whyChooseUs from '..assets/images/why1.png'
+import 
 
-const 
+const WhyChooseUs = () => {
+    return (
+        <section className="whychooseus-section">
+            <div className="whychooseus-card1">
+                <h1>
+                    Smart Filtering Algorithm
+                </h1>
+                <p className="content1">
+                    Automatically recommend the best startup locations based on your industry, budget, foot traffic, and more.
+                </p>
+
+                <a href="/">Learn how it works</a>
+
+            </div>
+        </section>
+
+    );
+};
+
+export default WhyChooseUs;
