6bd07536011d3ca32db8302a839f7b0c9a6ca840
Zhuowen-Chen <zhuowen.chen@ucdconnect.ie>
2025-06-21 23:02:08 +0100

    edit js file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+// Used to render a React application to the web page.
+
 import React from 'react';
+// Import ReactDOM to mount React elements onto the real DOM.
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
